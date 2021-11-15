18899f494b90540ca09d726051b13a17b18b58e4
Lucas Hoffman <75821031+lucash0331@users.noreply.github.com>
2021-11-14 20:49:35 -0500

    Update app.js

--- week-1/app.js
+++ week-1/app.js
@@ -1,45 +1,59 @@
 /*
-Title: web-340
+Title: App.js
 Author: Lucas Hoffman
 Date: 10/22/2021
 Description: App.js file for assignment 1.3
 */
 
-// jslint node: true 
-"use strict";
+// Requirement statements
+var express = require("express");
+var http = require("http");
+var swaggerUIExpress = require("swagger-ui-express");
+var swaggerJSDoc = require("swagger-jsdoc");
+var mongoose = require("mongoose");
 
-// Require statements
-const express = require('express');
-const http = require('http');
-const swaggerUi = require('swagger-ui-express');
-const swaggerJSDoc = require('swagger-jsdoc');
-const mongoose = require('mongoose');
-const logger = require('morgan');
+//Assigning Variable App to express library
+var app = express();
 
-// Assign var app to express library
-const app = express();
+//Setting the port to 3000
+app.set("port", process.env.PORT || 3000);
 
-// Set port
-app.set('port', process.env.PORT || 3000);
-
-// Parses JSON request objects
+//Set the app to use express.json
 app.use(express.json());
 
-// Parses incoming requests
-app.use(express.urlencoded({'extended': true}));
+//Set the app to use express.urlencoded
+app.use(express.urlencoded({ extended: true }));
 
-// Morgan logger
-app.use(logger("short"));
+//Connect to MongoDB
+var mongoDB = "mongodb+srv://web420_user:p455w0rd@buwebdev-cluster-1.umga8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, {});
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error: "));
+db.once("open", function () {
+  console.log("Application connected to MongoDB instance");
+});
 
-// Create an object literal.
-const options = {definition: {openApi: '3.0.0', info: {title: 'WEB 420 RESTful APIs', version: '1.0.0'}},
-        apis: ['./routes*.js']};
+//Defining object named options
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "WEB 420 RESTful APIs",
+      version: "1.0.0",
+    },
+  },
+  apis: ["./routes/*.js"], //files containing annotations for openAPI specifications.
+};
 
-        const openApiSpecification = swaggerJSDoc(options); // Creates new openApiSpec variable that calls the swaggerJsdoc library with the options literal.
+// Declaring openAPISpecification variable
+const openAPISpecification = swaggerJSDoc(options);
 
-        app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiSpecification)); // openApiSpecification variable is wired to the app variable.
-        
-        http.createServer(app).listen(app.get('port'), function () {
-            console.log('Application started on port ' + app.get('port')); // Starts the server listening on port 3000 using ('port') variable.
-        });
+//Declaring swaggerSpec variable
+app.use("/api-docs", swaggerUIExpress.serve, swaggerUIExpress.setup(openAPISpecification));
+
+//Create server and listen on port 3000.
+http.createServer(app).listen(app.get("port"), function () {
+  console.log("Application started and listening on port %s", +app.get("port"));
+});
                
